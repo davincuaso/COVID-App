11f58927c52a09662f1c1d5aa418c9ea8cf240f0
Davin Cuaso <davinsean@gmail.com>
2020-03-15 21:34:01 +0800

    added screen setup and firebase auth dependency, also updated cloud firestore

---
+++ covid_test_app/lib/screens/authenticate/authenticate.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatefulWidget {
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('authenticate screen just so yk what screen u are on atm'),
+    );
+  }
+}
---
+++ covid_test_app/lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:covid_test_app/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //should show whether or not logged in or not
+    return Home();
+  }
+}
