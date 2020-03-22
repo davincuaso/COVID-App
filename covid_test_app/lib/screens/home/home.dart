e7219c7a9aeba7b5c2165941a4b4b4859057b547
Davin Cuaso <davinsean@gmail.com>
2020-03-22 16:34:50 +0800

    used dummy data for creating a list view of users health result

--- covid_test_app/lib/screens/home/home.dart
+++ covid_test_app/lib/screens/home/home.dart
@@ -1,18 +1,28 @@
 import 'package:covid_test_app/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:covid_test_app/services/database.dart';
+import 'health_report.dart';
+import 'package:covid_test_app/models/form.dart';
 
 class Home extends StatelessWidget {
 
   final _auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(onPressed: () async { await _auth.signOut();}, icon: Icon(Icons.person), label: Text('Logout')),
-        ],
+    //this reflects the form results
+    return StreamProvider<List<FormResult>>.value(
+          value: DataBaseService().forms,
+          child: Scaffold(
+        appBar: AppBar(
+          title: Text('Home Page'),
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(onPressed: () async { await _auth.signOut();}, icon: Icon(Icons.person), label: Text('Logout')),
+          ],
+        ),
+        //make the HealthReport function something that happens when onPressed the health report (results)
+        body: HealthReport(),
       ),
     );
   }
--- covid_test_app/lib/services/database.dart
+++ covid_test_app/lib/services/database.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:covid_test_app/models/form.dart';
 
 class DataBaseService {
 
@@ -14,6 +15,22 @@ class DataBaseService {
       'name' : name,
       'healthResult' : healthResult,
     });
+  }
+
+  //gets a form list from the snapshot
+  List<FormResult> _formListFromSnapshot(QuerySnapshot snapshot) {
+    //how to get the documents within the snapshot
+    //the map function maps the documents into another iterable
+    //returning a new iterable of several forms dependent on what we have in the collection
+    return snapshot.documents.map((doc){
+      return FormResult(name: doc.data['name'] ?? '', healthResult: doc.data['healthResult'] ?? '');
+    }).toList(); //this is to change it from an iterable to a list
+  }
+
+  //get the form stream of the firestore collection
+  Stream<List<FormResult>> get forms {
+    //this is a method using the firestore library
+    return formCollection.snapshots().map(_formListFromSnapshot);
   }
 
 }
